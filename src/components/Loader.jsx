436745341fcd9a2aeaf1a1e1867c0160a0a43d40
theprabhanjan <prabhanjandhobale@outlook.com>
2024-11-19 15:45:53 +0530

    fixed loader issue

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Loader() {
   return (
     <div id="loader" className='border-b-cyan-500 '>
-      <h1 className='text-[10vw]'>Hi! I'M Prabhanjan</h1>
-      <h1>Welcome to my Portfolio</h1>
+      <h1 className='sm:text-[5vw] text-[12vw]'>Hi! I'M Prabhanjan</h1>
+      <h1 className='sm:text-[5vw] text-[12vw]'>Welcome to my Portfolio</h1>
     </div>
   )
 }
