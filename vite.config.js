ebe6ca7eed16cf9069c2a9b973658a38b86d56b5
theprabhanjan <prabhanjandhobale@outlook.com>
2024-12-04 11:47:34 +0530

    histroy api fallback

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server:{
     port:5173,
-    host:'0.0.0.0'
+    host:'0.0.0.0',
+    historyApiFallback: true, 
   },
   build: {
     outDir: 'dist',       // Output directory for the production build
